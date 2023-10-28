bb2ef41c17aa246af414552c6747be845326b21b
dan.sweeney <dan.sweeney@citilogik.com>
2023-10-28 17:07:01 +0100

    simple api request from frontend and printing data in plain text

---
+++ frontend/src/api/requests.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+const baseUrl = import.meta.env.VITE_API_URL;
+
+export type ConsolidatedDataResponse = {
+  energy: {
+    timestamp: string,
+    consumption: number,
+    isAnomaly: number
+  },
+  weather: {
+    timestamp: string,
+    averagetemperature: number,
+    averagehumidity: number
+  }
+};
+
+export function useConsolidatedDataRequest() {
+  const url = `${baseUrl}/consolidateddata`;
+
+  const [data, setData] = useState<ConsolidatedDataResponse | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const doRequest = async () => {
+    setLoading(true);
+    setData(null);
+    setError(false);
+
+    try {
+      const res = await fetch(url);
+
+      if (!res.ok) setError(true);
+      else {
+        const json = await res.json();
+        setData(json as ConsolidatedDataResponse)
+      }
+    } catch (err) {
+      console.error(err);
+      setError(true);
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    doRequest();
+  }, []);
+
+  return { data, loading, error, doRequest };
+}
